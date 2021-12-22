49a0c4cf3f36945b2eadf3a56dde8b515022e7db
SunniDaze7 <pnykii98@gmail.com>
2021-12-22 16:09:14 -0600

    Added computer selection, and part of play round function

--- src/index.js
+++ src/index.js
@@ -11,3 +11,31 @@
 //use prompt to get user input
 //dont consoloe.log in any function except displaying results in game()
 
+const selections = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    let randomSelection = selections[Math.floor(Math.random()*selections.length)];
+    return randomSelection;
+}
+
+function playRound(playerSelection, computerSelection){
+let playerSelection = prompt("Make your Selction:", "Rock, Paper, Or Scissors");
+let computerSelection = computerPlay();
+if (computerSelection == "Rock" || playerSelection == "Paper") {
+    return "You win! Paper beats Rock.";
+} else if (computerSelection == "Paper" || playerSelection == "Rock") {
+    return "You lose! Paper beats Rock.";
+} else if (computerSelection == "Rock" || playerSelection == "Scissors") {
+    return "You lose! Rock beats Scissors.";
+} else if (computerSelection == "Scissors" || playerSelection == "Rock") {
+    return "You Win! Rock beats Scissors.";
+} else if (computerSelection == "Scissors" || playerSelection == "Paper") {
+    return "You lose! Scissors beats Paper."
+} else if (computerSelection == "Paper" || playerSelection == "Scissors") {
+    return "You Win! Scissors beats Paper."
+} else if (computerSelection == playerSelection) {
+    return "It's a Draw!";
+}
+}
+
+//work on case insensitive
