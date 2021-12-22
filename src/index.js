1bae0183c1b140ae5f8491eaaa17172fe0d3319e
SunniDaze7 <pnykii98@gmail.com>
2021-12-22 13:13:36 -0600

    Added program notes for set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+//create randomize function called computerPlay() "Rock, paper or Scissors"
+
+//write function called playRound() to play one round
+//takes 2 parameters playerSelection and computerSelection(case-insensitive)
+//returns string such as "You Lose! Paper Beats Rock"
+
+//write function called game() to play five rounds by
+//looping in function playRound() and keep score as well as
+//report winner or loser
+
+//use prompt to get user input
+//dont consoloe.log in any function except displaying results in game()
+
