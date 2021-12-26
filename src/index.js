7df3b78f6abd3b599c4df47a0ac67d42f63f9619
SunniDaze7 <pnykii98@gmail.com>
2021-12-26 13:26:05 -0600

    Finished playRound() function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 //write function called playRound() to play one round
 //takes 2 parameters playerSelection and computerSelection(case-insensitive)
-//returns string such as "You Lose! Paper Beats Rock"
+//console.log(s string such as "You Lose! Paper Beats Rock"
 
 //write function called game() to play five rounds by
 //looping in function playRound() and keep score as well as
@@ -11,31 +11,31 @@
 //use prompt to get user input
 //dont consoloe.log in any function except displaying results in game()
 
-const selections = ["Rock", "Paper", "Scissors"];
+const selections = ["rock", "paper", "scissors"];
 
 function computerPlay() {
     let randomSelection = selections[Math.floor(Math.random()*selections.length)];
-    return randomSelection;
+   return randomSelection;
 }
 
 function playRound(playerSelection, computerSelection){
-let playerSelection = prompt("Make your Selction:", "Rock, Paper, Or Scissors");
-let computerSelection = computerPlay();
-if (computerSelection == "Rock" || playerSelection == "Paper") {
-    return "You win! Paper beats Rock.";
-} else if (computerSelection == "Paper" || playerSelection == "Rock") {
-    return "You lose! Paper beats Rock.";
-} else if (computerSelection == "Rock" || playerSelection == "Scissors") {
-    return "You lose! Rock beats Scissors.";
-} else if (computerSelection == "Scissors" || playerSelection == "Rock") {
-    return "You Win! Rock beats Scissors.";
-} else if (computerSelection == "Scissors" || playerSelection == "Paper") {
-    return "You lose! Scissors beats Paper."
-} else if (computerSelection == "Paper" || playerSelection == "Scissors") {
-    return "You Win! Scissors beats Paper."
-} else if (computerSelection == playerSelection) {
-    return "It's a Draw!";
-}
-}
+    if (computerSelection == "rock" || playerSelection == "paper") {
+       return "You win! Paper beats Rock.";
+    } else if (computerSelection == "paper" || playerSelection == "rock") {
+       return "You lose! Paper beats Rock.";
+    } else if (computerSelection == "rock" || playerSelection == "scissors") {
+       return "You lose! Rock beats Scissors.";
+    } else if (computerSelection == "scissors" || playerSelection == "rock") {
+       return "You Win! Rock beats Scissors.";
+    } else if (computerSelection == "scissors" || playerSelection == "paper") {
+       return "You lose! Scissors beats Paper.";
+    } else if (computerSelection == "paper" || playerSelection == "scissors") {
+       return "You Win! Scissors beats Paper.";
+    } else if (computerSelection == playerSelection) {
+       return "It's a Draw!";
+    }
+    }
 
-//work on case insensitive
+    
+//wrap this console log in a function called game(), and repeat function calls
+console.log(playRound(prompt("Rock, Paper, or Scissors?").toLowerCase(), computerPlay()));
